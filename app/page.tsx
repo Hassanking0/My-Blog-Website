cc80174d7a291e1a3ad040c7c468ca6c67b0bcc3
Apple <apple@Apples-MacBook-Air.local>
2025-09-19 00:15:25 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Mypic from "@/public/jpg/mypic.jpg"
 const HomePage = () => {
 	return (
 		<div>
@@ -134,6 +136,72 @@ const HomePage = () => {
 					</div>
 				</div>
 			</section>
+      {/* component */}
+ <div className="flex justify-center items-center min-h-screen">
+      <div className="max-w-[720px] mx-auto">
+       
+
+        {/* Testimonial Card */}
+        <div className="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-transparent text-gray-700 shadow-none">
+          {/* Header */}
+          <div className="relative flex items-center gap-4 pt-0 pb-8 mx-0 mt-4">
+            <Image
+              src={Mypic}
+              alt="Hassan King"
+              width={58}
+              height={58}
+              className="inline-block h-[58px] w-[58px] rounded-full object-cover"
+            />
+            <div className="flex w-full flex-col gap-0.5">
+              <div className="flex items-center justify-between">
+                <h5 className="text-xl font-semibold text-blue-gray-900">
+                  Hassan King
+                </h5>
+                <div className="flex items-center gap-0.5 text-yellow-700">
+                  {Array(5)
+                    .fill(0)
+                    .map((_, i) => (
+                      <svg
+                        key={i}
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-5 h-5"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M10.788 3.21c.448-1.077 
+                             1.976-1.077 2.424 0l2.082 5.007 
+                             5.404.433c1.164.093 1.636 1.545.749 
+                             2.305l-4.117 3.527 1.257 5.273c.271 
+                             1.136-.964 2.033-1.96 1.425L12 
+                             18.354 7.373 21.18c-.996.608-2.231-.29-
+                             1.96-1.425l1.257-5.273-4.117-3.527c-.887-
+                             .76-.415-2.212.749-2.305l5.404-.433 
+                             2.082-5.006z"
+                        />
+                      </svg>
+                    ))}
+                </div>
+              </div>
+              <p className="text-base font-light text-blue-gray-900">
+                Frontend Lead @ Google
+              </p>
+            </div>
+          </div>
+
+          {/* Body */}
+          <div className="p-0 mb-6">
+            <p className="text-base font-light leading-relaxed text-gray-700">
+              &quot;I found a solution to all my design needs from Creative Tim. I
+              use them as a freelancer in my hobby projects for fun! And it's
+              really affordable, very humble guys!!!&quot;
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
 		</div>
 	)
 }
